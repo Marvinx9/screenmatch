6d69a795efbc2f56cbe84625cf528ddf02833395
afranio dantas <afraniodantas0224@gmail.com>
2025-05-01 21:14:53 -0300

    feat: add dados-temporadas

---
+++ screenmatch/src/main/java/br/com/marvin/screenmatch/model/DadosEpisodio.java
@@ -0,0 +1,12 @@
+package br.com.marvin.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("Episode") Integer numero,
+        @JsonAlias("imdRating") String avaliacao,
+        @JsonAlias("Released") String dataLamcamento) {
+}
