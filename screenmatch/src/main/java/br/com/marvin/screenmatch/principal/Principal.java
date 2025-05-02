3be9d9fa1dc773a758736b05f2982d4505d4f9c6
afranio dantas <afraniodantas0224@gmail.com>
2025-05-01 22:17:14 -0300

    feat: add class principal

--- screenmatch/src/main/java/br/com/marvin/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/marvin/screenmatch/principal/Principal.java
@@ -26,11 +26,12 @@ public class Principal {
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
