c4473a33e3204be83b802255b4bc3a92b226c135
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2026-06-28 09:34:31 -0300

    feat: increment type serie in list serie findings

--- screenmatch/src/main/java/br/com/marvin/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/marvin/screenmatch/principal/Principal.java
@@ -2,10 +2,12 @@ package br.com.marvin.screenmatch.principal;
 
 import br.com.marvin.screenmatch.model.DadosSerie;
 import br.com.marvin.screenmatch.model.DadosTemporada;
+import br.com.marvin.screenmatch.model.Serie;
 import br.com.marvin.screenmatch.service.ConsumoApi;
 import br.com.marvin.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -79,6 +81,8 @@ public class Principal {
     }
 
     private void listarSeriesBuscadas() {
-        dadosSeries.forEach(System.out::println);
+        List<Serie> series = new ArrayList<>();
+        series = dadosSeries.stream().map(Serie::new).toList();
+        series.stream().sorted(Comparator.comparing(Serie::getGenero)).forEach(System.out::println);
     }
 }
